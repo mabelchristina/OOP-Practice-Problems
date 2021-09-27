69c70c1075e9808e0d3aa9c25741ca143917345d
Mabel Christina <maybelchristina@gmail.com>
2021-09-27 21:40:10 +0530

    [Mabel] Add: UC1 added

---
+++ OOPS Practice Problems/JSON Inventory Management/InventoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS_Practice_Problems.JSON_Inventory_Management
+{
+    class InventoryModel
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ OOPS Practice Problems/Program.cs
@@ -0,0 +1,24 @@
+﻿using OOPS_Practice_Problems.JSON_Inventory_Management;
+using System;
+
+namespace OOPS_Practice_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose 1.JSONInventoryDataManagement");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    InventoryManagementMain inventoryMain = new InventoryManagementMain();
+                    inventoryMain.DisplayData(@"D:\my git\OOP-Practice-Problems\OOPS Practice Problems\JSON Inventory Management\Inventory.json");
+                    break;
+                default:
+                    Console.WriteLine("Choose valid option");
+                    break;
+            }
+        }
+    }
+}
